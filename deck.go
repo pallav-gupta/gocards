bc815bfaac17ec7fa01255ca71a7ac4d640adbd2
Pallav Gupta <er.pallav.gupta@gmail.com>
2024-02-20 15:09:10 +0100

    added test

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // Create a new type of deck
 // it is a slice of strings
@@ -10,7 +15,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"} //, "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -32,4 +37,34 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	//source := rand.NewSource(time.Now().UnixNano())
+	//r := rand.New(source)
+
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		//newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 func main() {
 	cards := newDeck()
+	//cards := newDeckFromFile("my_cards")
+	//cards.saveToFile("my_cards")
+	//cards.shuffle()
+	cards.print()
+	//hand, remainingCards := cards.deal(5)
 
-	hand, remainingCards := deal(cards,5)
+	//hand.print()
+	//remainingCards.print()
 
-	hand.print()
-	remainingCards.print()
 }
