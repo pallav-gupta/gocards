f66a29037990baa549fcfadeb0e2b1bb0e9c86a0
Pallav Gupta <er.pallav.gupta@gmail.com>
2024-02-21 22:48:14 +0100

    intro to struct

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	//cards := newDeck()
 	//cards := newDeckFromFile("my_cards")
 	//cards.saveToFile("my_cards")
 	//cards.shuffle()
-	cards.print()
+	//cards.print()
 	//hand, remainingCards := cards.deal(5)
 
 	//hand.print()
 	//remainingCards.print()
+	//alex := person{"Alex", "Anderson"}
+	bob := person{
+		firstName: "Bob",
+		lastName:  "Max",
+		contact: contactInfo{
+			email:   "aaa@gmail.com",
+			zipCode: 93000,
+		},
+	}
+
+	//fmt.Println(alex)
+	//var alex person
+	//alex.firstName = "Alex"
+
+	fmt.Println(bob)
+	//fmt.Printf("%+v\n", bob)
+	bob.print()
+
+	bob.updateName("Jim")
+	bob.print()
+	bobPointer := &bob
+	fmt.Println(bobPointer)
+	bobPointer.contact.email = "abcd@gmail.com"
+	bob.print()
 
 }
---
+++ person.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (pointerToPerson *person) updateName(firstName string) {
+	pointerToPerson.firstName = firstName
+}
